3441797824d5b83af69f85b8018168f870f60131
André Scafi <scafi.andre@gmail.com>
2024-12-12 14:46:34 -0300

    Version 1.1: Change the screen transition

--- app/src/main/java/com/scafisystems/myridesimulator/ui/navigation/NavigationGraph.kt
+++ app/src/main/java/com/scafisystems/myridesimulator/ui/navigation/NavigationGraph.kt
@@ -3,17 +3,12 @@ package com.scafisystems.myridesimulator.ui.navigation
 import androidx.activity.compose.BackHandler
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.core.tween
-import androidx.compose.animation.expandIn
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.shrinkOut
 import androidx.compose.animation.slideInHorizontally
-import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutHorizontally
-import androidx.compose.animation.slideOutVertically
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
 import com.google.accompanist.navigation.animation.AnimatedNavHost
 import com.google.accompanist.navigation.animation.composable
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
@@ -33,58 +28,56 @@ fun NavigationGraph() {
         navController = navController,
         startDestination = Routes.HomeScreen.name
     ) {
-        composable(
+
+        composableWithAnimation(
             route = Routes.HomeScreen.name,
-            enterTransition = {
-                slideInHorizontally(initialOffsetX = { -1000 }, animationSpec = tween(700))
-            },
-            exitTransition = {
-                slideOutHorizontally(targetOffsetX = { 1000 }, animationSpec = tween(700))
-            }
+            navController = navController
         ) {
             HomeScreen(navController = navController)
         }
-        composable(
+
+        composableWithAnimation(
             route = Routes.TripRequestScreen.name,
-            enterTransition = {
-                fadeIn(animationSpec = tween(500))
-            },
-            exitTransition = {
-                fadeOut(animationSpec = tween(500))
-            }
+            navController = navController
         ) {
-            BackHandler(enabled = true) {
-                navController.popBackStack()
-            }
             RequestTripScreen(navController = navController, viewModel = viewModel)
         }
-        composable(
+
+        composableWithAnimation(
             route = Routes.TripHistoryScreen.name,
-            enterTransition = {
-                expandIn(expandFrom = Alignment.BottomCenter, animationSpec = tween(500))
-            },
-            exitTransition = {
-                shrinkOut(shrinkTowards = Alignment.TopCenter, animationSpec = tween(500))
-            }
+            navController = navController
         ) {
-            BackHandler(enabled = true) {
-                navController.popBackStack()
-            }
             HistoryTripScreen(navController = navController, viewModel = viewModel)
         }
-        composable(
+
+        composableWithAnimation(
             route = Routes.TripOptionsScreen.name,
-            enterTransition = {
-                slideInVertically(initialOffsetY = { 1000 }, animationSpec = tween(700))
-            },
-            exitTransition = {
-                slideOutVertically(targetOffsetY = { -1000 }, animationSpec = tween(700))
-            }
+            navController = navController
         ) {
-            BackHandler(enabled = true) {
-                navController.popBackStack()
-            }
             TripOptionsScreen(navController = navController, viewModel = viewModel)
         }
+    }
+}
+
+@OptIn(ExperimentalAnimationApi::class)
+fun NavGraphBuilder.composableWithAnimation(
+    route: String,
+    navController: NavHostController,
+    screen: @Composable () -> Unit,
+
+    ) {
+    return composable(
+        route = route,
+        enterTransition = {
+            slideInHorizontally(initialOffsetX = { 1000 }, animationSpec = tween(700))
+        },
+        exitTransition = {
+            slideOutHorizontally(targetOffsetX = { -1000 }, animationSpec = tween(700))
+        }
+    ) {
+        BackHandler(enabled = true) {
+            navController.popBackStack()
+        }
+        screen()
     }
 }
